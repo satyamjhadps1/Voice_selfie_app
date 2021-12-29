b7bdf73769193bc6f2709dbdbcf26b68607b5562
satyamjhadps1 <84779436+satyamjhadps1@users.noreply.github.com>
2021-12-29 14:57:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function(event){
+    console.log(event);
+    content = event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML = content;
+    speak();
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    var speak_data = document.getElementById("textbox").value;
+    var  utter_this = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+    Webcam.attach(camera)
+}
+Webcam.set({
+    width : 360,
+    height : 250,
+    image_format : "png",
+    png_quality : 90
+});
+camera = document.getElementById("camera");
+
